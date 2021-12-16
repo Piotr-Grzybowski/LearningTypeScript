8744735f965bb2cd558d2eb841a51ae421d778b5
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2021-12-16 21:32:27 +0100

    Add Tsconfig file

---
+++ JS/1.WhatIsTypeScript.js
@@ -0,0 +1,16 @@
+/**
+ * 1. What is TypeScript?
+ * Simply put, TypeScript is an open-source programming language developed by Microsoft that compiles to JavaScript. Since its release in 2012, the language has remained in active development and continues to gain in popularity every year.
+ * The development community strongly associated TypeScript with Angular in the early days, despite them being distinct creations from the start. That was because Angular forces you to use TypeScript, though bypassing this is possible.
+ * The most important difference between TypeScript and JavaScript is that they are two separate programming languages, though TypeScript is heavily based on JavaScript. In fact, TypeScript is a superset of JavaScript, meaning all valid JavaScript code is also valid TypeScript code.
+TypeScript doesn’t alter JavaScript, instead expanding it with new, valuable features. Everything that can be written in JavaScript can also be written in TypeScript.
+*
+* 2. How does TypeScript improve JavaScript?
+* The key augmentation TypeScript brings to JavaScript is a type system (hence the name “TypeScript”).
+
+In case you’re unfamiliar with the word, “type” is a definition that tells you the intended use of given data (for instance, we have numeric, Boolean, or string types, just to name a few).
+
+Traditionally, JavaScript is dynamically typed (much like Python). This means that a single variable can either contain text, a number, or even a whole database entity, depending on the occasion—and that’s just fine. What TypeScript does is strictly define what a given variable can contain.
+
+Let’s say one of your developers writes an addNumbers function and uses types to define that the function can only take two numbers. TypeScript will then return an error if another developer tries to provide text to the function, whereas in JavaScript such an operation would be perfectly acceptable.
+ */
